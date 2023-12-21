5432f1997e8c7016012155ebbeacfaa642afe92c
kira924age <kira924age@gmail.com>
2023-12-21 21:28:29 +0900

    feat: 🎸 update decodePayload

--- src/index.ts
+++ src/index.ts
@@ -199,11 +199,15 @@ async function importKey(key: string | JsonWebKey, algorithm: SubtleCryptoImport
 function decodePayload<T = any>(raw: string): T | undefined {
     try {
         const binaryString = atob(raw)
-        const encodedString = encodeURIComponent(binaryString).replace(/%([0-9A-F]{2})/g, (_match, p1) => {
-          return String.fromCharCode(parseInt(p1, 16));
-        });
+        const bytes = new Uint8Array(binaryString.length);
+        for (let i = 0; i < binaryString.length; i++) {
+            bytes[i] = binaryString.charCodeAt(i);
+        }
 
-        return JSON.parse(decodeURIComponent(encodedString));
+        const decoder = new TextDecoder('utf-8');
+        const decodedString = decoder.decode(bytes);
+
+        return JSON.parse(decodedString);
     } catch {
         return
     }
